2f9551c9cfdaa716c9b2f7cb7b3feeed4c5cd058
piastu <s.ergeystaroletov@gmail.com>
2015-05-05 08:31:29 +0600

    first test

---
+++ LaunchProgram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace ProjectTdd
+{
+    public class LaunchProgram
+    {
+        public string Launching(string p)
+        {
+            var process = Process.Start(p);
+            return process != null ? process.ProcessName : null;
+        }
+    }
+}
